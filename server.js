ee4c35a72524797164877f2707173560df2229c3
Sri (Laptop) <sridharkaza@yahoo.com>
2019-01-26 13:51:30 -0800

    added favicon

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 'use strict';
 
-const express = require('express')
+const express = require('express');
 
 const app = express();
+app.use(express.static('public'));
 app.get('/',(req,res)=>{
     res.sendfile('index.html');
 });
