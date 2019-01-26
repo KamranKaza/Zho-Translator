50e4d1c28c4fa3260216b18a0b4adc4cdc841d25
Sri (Laptop) <sridharkaza@yahoo.com>
2019-01-26 13:01:12 -0800

    changed server to express

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 var http = require('http');
 
-var server = http.createServer(function(request, response) {
+//var server = http.createServer(function(request, response) {
+//    response.sendfile('translator.html');
+//});
+// var port = process.env.PORT || 1337;
+//server.listen(port);
 
-    response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Hello World!");
-
-});
-
-var port = process.env.PORT || 1337;
-server.listen(port);
-
+const express = require('express');
+const app = new express();
+app.get("/",function(request,response){
+    response.sendFile('translator.html');
+})
 console.log("Server running at http://localhost:%d", port);
